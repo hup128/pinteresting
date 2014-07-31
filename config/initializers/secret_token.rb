e735702939e70e5246adcf9b2e9e8c527ecfb6fb
hoofar <hup128@psu.edu>
2014-07-30 22:53:49 -0400

    server rails fixed

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Pinteresting::Application.config.secret_token = 'ffe44bf753402fcccabf7f2569143ca7d667c0537f1de30a98f56dd79917852c07ce7426c88ca6523939d556086ba39bd6a4176b3e627b136710e4d50567aeff'
+Pinteresting::Application.config.secret_token = '0c20579356c7f036ddcf821fa7bc9002dc9b396582a1fbffd569a8a045f2f1f6acd87d03b876ef8747d0c0e6a939e3e99e3c549acdf9b3472138d2824e5993a9'
